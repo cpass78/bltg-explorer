44ff6b10b4dd665351afcf76c4b12308eb3baa12
Chris Passarella <chris@block-logic.com>
2019-11-20 23:18:39 -0500

    Update Card Links

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -5,15 +5,15 @@ import Card from './Card';
 
 const CardLinks = () => (
   <Card title="Links">
-    <a href="https://bulwarkcrypto.com/" target="_blank" rel="nofollow noopener">Website</a><br />
-    <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank" rel="nofollow noopener">Bitcointalk</a><br />
-    <a href="https://github.com/bulwark-crypto" target="_blank" rel="nofollow noopener">Github</a><br />
-    <a href="https://www.reddit.com/r/BulwarkCoin/" target="_blank" rel="nofollow noopener">Reddit</a><br />
-    <a href="https://discord.gg/a7vhegP" target="_blank" rel="nofollow noopener">Discord</a><br />
-    <a href="https://t.me/bulwarkcrypto" target="_blank" rel="nofollow noopener">Telegram</a><br />
-    <a href="https://twitter.com/BulwarkCrypto" target="_blank" rel="nofollow noopener">Twitter</a><br />
-    <a href="https://medium.com/bulwark-cryptocurrency" target="_blank" rel="nofollow noopener">Medium</a><br />
-    <a href="https://community.bulwarkcrypto.com/" target="_blank" rel="nofollow noopener">Community Forum</a>
+    <a href="https://block-logic.com/" target="_blank" rel="nofollow noopener">Website</a><br />
+    {/*<a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank" rel="nofollow noopener">Bitcointalk</a><br />*/}
+    <a href="https://github.com/Block-Logic-Technology-Group" target="_blank" rel="nofollow noopener">Github</a><br />
+    {/*<a href="https://www.reddit.com/r/BulwarkCoin/" target="_blank" rel="nofollow noopener">Reddit</a><br />*/}
+    <a href="https://discord.gg/cWUSgUE" target="_blank" rel="nofollow noopener">Discord</a><br />
+    <a href="https://t.me/bltgx" target="_blank" rel="nofollow noopener">Telegram</a><br />
+    <a href="https://twitter.com/BlockLogicTech" target="_blank" rel="nofollow noopener">Twitter</a><br />
+    <a href="https://medium.com/@BlockLogic" target="_blank" rel="nofollow noopener">Medium</a><br />
+    <a href="https://forum.block-logic.com/" target="_blank" rel="nofollow noopener">Community Forum</a>
   </Card>
 );
 
