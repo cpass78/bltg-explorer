d67272e890158e2c807bc1751bbd57bac8d77b20
Chris Passarella <chris@block-logic.com>
2019-11-20 23:38:56 -0500

    Update Exchange Links

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -5,13 +5,9 @@ import Card from './Card';
 
 const CardExchanges = () => (
   <Card title="Exchanges">
-    <a href="https://crypto-bridge.org/" target="_blank" rel="nofollow noopener">CryptoBridge</a><br />
-    <a href="https://txbit.io/Trade/BWK/BTC" target="_blank" rel="nofollow noopener">Txbit.io</a><br />
-    <a href="https://blocknet.co/" target="_blank" rel="nofollow noopener">Blocknet</a><br />
-    <a href="https://www.altilly.com/market/BWK_BTC" target="_blank" rel="nofollow noopener">altilly</a><br />
-    <a href="https://bastionex.com/" target="_blank" rel="nofollow noopener">Bastion</a><br />
-    <a href="https://green-dex.net/market/GREENDEX.BWK_GREENDEX.BTC" target="_blank" rel="nofollow noopener">GreenDex</a><br />
-    <a href="https://novaexchange.com/market/BTC_BWK/" target="_blank" rel="nofollow noopener">NovaExchange</a><br />
+    <a href="https://www.southxchange.com/Market/Book/BLTG/BTC" target="_blank" rel="nofollow noopener">SouthXChange</a><br />
+    <a href="https://app.stex.com/en/trade/pair/BTC/BLTG" target="_blank" rel="nofollow noopener">Stex</a><br />
+    <a href="https://www.finexbox.com/market/pair/BLTG-BTC.html" target="_blank" rel="nofollow noopener">Finexbox</a><br />
   </Card>
 );
 
