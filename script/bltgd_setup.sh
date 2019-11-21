7847c8b94855015bb4943fed5f5a0dd248b93a76
Chris Passarella <chris@block-logic.com>
2019-11-20 21:08:55 -0500

    Update naming for bltg

--- script/bltgd_setup.sh
+++ script/bltgd_setup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # Download latest node and install.
-bwklink=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
-mkdir -p /tmp/bulwark
-cd /tmp/bulwark
-curl -Lo bulwark.tar.gz $bwklink
-tar -xzf bulwark.tar.gz
+bltglink=`curl -s https://api.github.com/repos/Block-Logic-Technology-Group/bltg/releases/latest | grep browser_download_url | grep x86_64-linux | cut -d '"' -f 4`
+mkdir -p /tmp/bltg
+cd /tmp/bltg
+curl -Lo bltg.tar.gz $bltglink
+tar -xzf bltg.tar.gz
 sudo mv ./bin/* /usr/local/bin
 cd
-rm -rf /tmp/bulwark
-mkdir ~/.bulwark
+rm -rf /tmp/bltg
+mkdir ~/.bltg
 
 # Setup configuration for node.
 rpcuser=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-cat >~/.bulwark/bulwark.conf <<EOL
+cat >~/.bltg/bltg.conf <<EOL
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
+server=1
 daemon=1
 txindex=1
 EOL
 
 # Start node.
-bulwarkd
+bltgd
